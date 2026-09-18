eb205d5657c946313428e53d1c7760f1e32c1888
jackintheact <jacobgalvan519@gmail.com>
2026-09-18 18:05:07 +0000

    update

--- Java/000_Labs/010_random_method-static/base_code/starter.java
+++ Java/000_Labs/010_random_method-static/base_code/starter.java
@@ -11,5 +11,12 @@ class starter {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
 		System.out.print("I love to learn coding remotely."); 
+	    double myDouble = 0.98;
+		int myInt = (0.98) myDouble;
+		System.out.println(0.98);
+		
+		
+        
+	
 	}
 }
--- Java/102_Exercises/09_Next5/base_code/starter.java
+++ Java/102_Exercises/09_Next5/base_code/starter.java
@@ -7,7 +7,13 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a number");
+	  int num = sc.nextInt();
+	  System.out.println(num + ", " + (num+1) + (num+2) + (num+3) + (num+4) + (num+5));
 
-
+	  System.out.println(num*2 + ", " + (num*2) + ", " + (num*3) + ", " + (num*4));
+	  System.out.println((num+0.0)/100.0);
+	  System.out.println((num+0.0)/10.0);
 	}
 }
