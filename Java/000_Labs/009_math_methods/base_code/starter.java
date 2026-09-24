a784b26a87e7c1911c4ec321be1aa0c834cd9351
jackintheact <jacobgalvan519@gmail.com>
2026-09-24 18:00:28 +0000

    update

--- Java/000_Labs/009_math_methods/base_code/starter.java
+++ Java/000_Labs/009_math_methods/base_code/starter.java
@@ -10,5 +10,10 @@ class starter {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
 		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		I love pancakes = sc.nextLine();
+		System.out.print("I love pancakes.");
+		Math.pow(2,4);
+
 	}
 }
